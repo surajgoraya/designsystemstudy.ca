e35b402149d0a6387c335db9d34533717f92e741
surajgoraya <SurajGoraya@outlook.com>
2023-11-22 11:40:30 -0500

    fix: :mag: seo cache doing weird things

--- src/lib/components/Template.svelte
+++ src/lib/components/Template.svelte
@@ -22,7 +22,7 @@
 			'At what point do design systems become viable for organizations to implement? What does the future of design systems look like? To answer these questions this study at the University of Guelph will set out to interview those who build, maintain, or use design systems across a variety of organization sizes.',
 		images: [
 			{
-				url: 'https://designsystemstudy.ca/ogp/ogp1.png',
+				url: 'https://designsystemstudy.ca/ogp/ogp.png',
 				width: 1920,
 				height: 1080,
 				alt: 'A black and white image displaying the text "design system study." on the bottom left, in white and the text "University of Guelph" on the top right.'
