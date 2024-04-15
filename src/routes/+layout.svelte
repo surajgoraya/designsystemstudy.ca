ecf0c6099e87394897e919f97e1e11be14697168
surajgoraya <SurajGoraya@outlook.com>
2024-04-15 11:57:28 -0400

    Revert "feat: temp pause on study"
    This reverts commit 27148967f47226ea1b8f5554e977fe8351b0b340.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,7 @@
   import SkipToMain from '$lib/components/SkipToMain.svelte';
 </script>
 
-<!-- <SkipToMain /> -->
-<h1>Thanks for checking in,</h1>
-Intake for this study has been paused, check back later!
-<br/>
-<br/>
-<code>&dash; See you soon!</code>
-<!-- <Nav />
+<SkipToMain />
+<Nav />
 <slot />
-<Footer /> -->
+<Footer />
